ef58071719f2531a04016e9776331ba91082d6eb
daddidisney <htillustrator@gmail.com>
2018-02-17 21:09:27 -0700

    02-01-2018 First time in project

--- src/main/java/Controller/supplyWeightController.java
+++ src/main/java/Controller/supplyWeightController.java
@@ -27,11 +27,10 @@ double weight;
     // JOptionPane returns data-type String.
     double amount = Double.parseDouble(input);
 
-    //Calculate weight
+    //Calculates weight
     weight = amount * 10;
 
-    //Design decision structure to determine if 
-    //weight is 'too heavy' or 'too light'.
+    //The if cases to display if supply is too heavy, not enough, or just right'.
 
     if(weight > 500){
     throw new Exception(null, "Supply exceeds weight limit!");
@@ -40,7 +39,7 @@ double weight;
     else if (weight < 1) {
         throw new Exception(null, "Supply is depleted.");
     }
-    // Optional branch
+
     else {
     throw new Exception(null, "Supplies do not exceed weight limit!");
     }

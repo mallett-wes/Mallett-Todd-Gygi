f0f3205a30a8ef1e9908d59bd7aba132f93ab139
daddidisney <htillustrator@gmail.com>
2018-02-17 21:01:59 -0700

    02-01-2018 First time in project

---
+++ src/test/java/Controller/supplyWeightControllerTest.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import model.Supplies;
+import model.Team;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author hiramtodd
+ */
+public class supplyWeightControllerTest {
+    
+    public supplyWeightControllerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of supplyWeight method, of class supplyWeightController.
+     */
+    @Test
+    public void testSupplyWeight() throws Exception {
+        System.out.println("supplyWeight");
+        Supplies supply = null;
+        int quantity = 0;
+        Team team = null;
+        supplyWeightController instance = new supplyWeightController();
+        Supplies expResult = null;
+        Supplies result = instance.supplyWeight(supply, quantity, team);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}

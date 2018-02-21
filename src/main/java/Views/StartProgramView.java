6faee9715e88ae2f90e51fe3c4d46b8c7e4e390b
wesmallett <wesmallett@192.168.0.6>
2018-02-20 19:59:43 -0700

    team assignment first pass

--- src/main/java/Views/StartProgramView.java
+++ src/main/java/Views/StartProgramView.java
@@ -1,14 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Views;
 
-/**
- *
- * @author hiramtodd
- */
+import Controller.CharacterController;
+import model.Character;
+
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class StartProgramView {
-    
+    Scanner scanner = new Scanner(new InputStreamReader(System.in));
+
+    public StartProgramView(){
+
+    }
+
+    public void displayStartProgramView(){
+        doAction(getInputs());
+    }
+
+    private String[] getInputs(){
+        String[] input = new String[1];
+        System.out.println("Welcome to Mormon Trek!");
+        System.out.println("Please enter the name of your character: ");
+        String result = scanner.nextLine().trim().toLowerCase();
+
+        while(result.length() < 1){
+            System.out.println("You must enter a non-blank value or enter q to quit.");
+            result = scanner.nextLine().trim().toLowerCase();
+
+            if(result.equals("q")) {
+                System.exit(0);
+            }
+        }
+
+        input[0] = result;
+
+        return input;
+    }
+
+    private boolean doAction(String[] inputs){
+        String mainCharacterName = inputs[0].toString();
+        CharacterController characterController = new CharacterController();
+        try{
+            characterController.selectMainCharacter(mainCharacterName);
+        }catch (Exception e){
+            System.out.println(e.getMessage().toString());
+            displayStartProgramView();
+        }finally{
+            System.out.println("Welcome to the game, " + mainCharacterName + "!");
+            System.out.println("We hope you have lots of fun!");
+        }
+        MainMenuView mainMenuView = new MainMenuView();
+        return true;
+
+    }
 }

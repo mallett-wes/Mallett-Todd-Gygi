8f0236800ce666d76812f793f74ce7a6b93b24db
daddidisney <htillustrator@gmail.com>
2018-02-22 21:48:27 -0700

    Adding to Master Branch
    Adding StartExistingGameView and GameView.

---
+++ src/main/java/Views/StartExistingGameView.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Views;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author hiramtodd
+ */
+public class StartExistingGameView {
+    
+    public void display() {
+        System.out.println("Welcome back! Would you like to continue an existing game?");
+    }
+}
